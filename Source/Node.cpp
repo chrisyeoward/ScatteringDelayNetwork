92e47b15d760c2c74a7f3cc59c0aa0867baa0891
chrisyeoward <chris.yeoward@btinternet.com>
2019-03-15 14:15:46 +0000

    Initialises temporary vector in scatter

--- Source/Node.cpp
+++ Source/Node.cpp
@@ -17,7 +17,7 @@ namespace SDN {
 		
 		for(int i = 0; i < delayOrder * delayOrder; i++) {
 			scatteringMatrix[i] = 2.0 / (float) delayOrder;
-			
+
 			if(i % (delayOrder + 1) == 0) scatteringMatrix[i] -= 1.0;
 		}
 	}
@@ -36,8 +36,9 @@ namespace SDN {
 		float tempVector[delayOrder];
 	
 		for(int i = 0; i < delayOrder; i ++) {
+			tempVector[i] = 0.0;
 			for(int j = 0; j < delayOrder; j++) {
-				tempVector[i] += scatteringMatrix[delayOrder*i + j] * (networkInput + waveVector[j]);
+				tempVector[i] += scatteringMatrix[(delayOrder * i) + j] * (networkInput + waveVector[j]);
 			}
 			tempVector[i] *= absorptionFactor;
 		}
