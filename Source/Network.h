f2914e5d5070ac58bc757e466c8518eab89c41a9
chrisyeoward <chris.yeoward@btinternet.com>
2019-03-16 17:38:26 +0000

    Edit room size

--- Source/Network.h
+++ Source/Network.h
@@ -26,12 +26,12 @@ namespace SDN
 		const int delayOrder = nodeCount - 1;
 		int connectionCount = 0;
 	
-		SDN::Point source = SDN::Point(0.5, 8.0);
-		SDN::Point mic = SDN::Point(1.2, 1.0);
+		SDN::Point source = SDN::Point(1.5, 4.0);
+		SDN::Point mic = SDN::Point(2.5, 1.0);
 	
 		SDN::Boundary bounds[nodeCount] = { SDN::Boundary(0.0, SDN::Orientation::Y),
-			SDN::Boundary(2.0, SDN::Orientation::Y),
-			SDN::Boundary(10.0, SDN::Orientation::X),
+			SDN::Boundary(5.0, SDN::Orientation::Y),
+			SDN::Boundary(5.0, SDN::Orientation::X),
 			SDN::Boundary(0.0, SDN::Orientation::X)
 		};
 		
